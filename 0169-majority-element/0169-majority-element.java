21dfd079bcbd7fec6b67666c5039b71fb4560945
SawaraJC <112013319+SawaraJC@users.noreply.github.com>
2024-07-02 16:56:46 +0530

    Time: 13 ms (30.01%), Space: 49.4 MB (79.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,16 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int majorityElement(int[] nums) {
-        // Create a HashMap to store the count of each element in the array.
-        Map<Integer, Integer> countMap = new HashMap<>();
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
 
-        // Populate the countMap with counts of each element.
-        for (int num : nums) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        for(int num: nums){
+            freqMap.put(num, freqMap.getOrDefault(num,0)+1);
         }
 
-        // Find the element with count greater than n/2.
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            if (entry.getValue() > nums.length / 2) {
+        for(Map.Entry<Integer,Integer> entry: freqMap.entrySet()){
+            if(entry.getValue() > nums.length/2)
                 return entry.getKey();
-            }
         }
 
-        // Return -1 if no majority element found.
         return -1;
     }
 }
