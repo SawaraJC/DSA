b1e903ab092a03c376160c0ea402dea25d20e0e4
SawaraJC <112013319+SawaraJC@users.noreply.github.com>
2024-08-19 15:11:44 +0530

    Time: 8 ms (98.33%), Space: 45 MB (68.47%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,24 +1,21 @@
-// import java.util.Arrays;
-
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        // Sort the greed factors and cookie sizes
         Arrays.sort(g);
         Arrays.sort(s);
-        
-        int count = 0; // To count the number of content children
-        int i = 0; // Pointer for greed factors (children)
-        int j = 0; // Pointer for cookie sizes
-        
-        // Use two-pointer technique to find the maximum content children
-        while (i < g.length && j < s.length) {
-            if (s[j] >= g[i]) {
+
+        int count = 0;
+
+        int i = 0;
+        int j = 0;
+
+        while(i < g.length && j < s.length){
+            if(s[j] >= g[i]){
                 count++;
                 i++;
             }
             j++;
         }
-        
+
         return count;
     }
 }
