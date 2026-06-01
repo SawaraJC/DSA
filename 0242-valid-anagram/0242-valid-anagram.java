4a4ace1695b010841f2fac872a7ede010133dc4a
SawaraJC <112013319+SawaraJC@users.noreply.github.com>
2026-06-01 22:24:44 +0530

    Time: 15 ms (25.15%), Space: 46.5 MB (32.36%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> sMap = freqMap(s);
+        HashMap<Character, Integer> tMap = freqMap(t);
+
+        return sMap.equals(tMap);
+    }
+
+    public HashMap<Character, Integer> freqMap(String s){
+        HashMap<Character, Integer> hmap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            hmap.merge(ch, 1, Integer::sum);
+        }
+
+        return hmap;
+    }
+}
