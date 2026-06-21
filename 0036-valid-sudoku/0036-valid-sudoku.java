1edb6db498474dcadf040244eba6ffc3ddcaf81c
SawaraJC <112013319+SawaraJC@users.noreply.github.com>
2026-06-21 12:51:46 +0530

    Time: 7 ms (31.81%), Space: 46.8 MB (31.06%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,24 +1,55 @@
-class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        HashSet<String> seen = new HashSet<>();
+// class Solution {
+//     public boolean isValidSudoku(char[][] board) {
+//         HashSet<String> seen = new HashSet<>();
         
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                char val = board[r][c];
+//         for (int r = 0; r < 9; r++) {
+//             for (int c = 0; c < 9; c++) {
+//                 char val = board[r][c];
 
-                if (val != '.') {
-                    String rowKey = val + " in row " + r;
-                    String colKey = val + " in col " + c;
-                    String blockKey = val + " in block " + (r / 3) + "-" + (c / 3);
+//                 if (val != '.') {
+//                     String rowKey = val + " in row " + r;
+//                     String colKey = val + " in col " + c;
+//                     String blockKey = val + " in block " + (r / 3) + "-" + (c / 3);
                     
 
-                    if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(blockKey)) {
-                        return false;
-                    }
+//                     if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(blockKey)) {
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
+
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        HashSet<String> set = new HashSet<>();
+
+        for(int r = 0; r < 9; r++) {
+
+            for(int c = 0; c < 9; c++) {
+
+                char val = board[r][c];
+
+                if(val == '.') {
+                    continue;
+                }
+
+                int box = (r / 3) * 3 + (c / 3);
+
+                if(!set.add(val + "r" + r) ||
+                   !set.add(val + "c" + c) ||
+                   !set.add(val + "b" + box))
+                {
+                    return false;
                 }
             }
         }
-        
+
         return true;
     }
 }
