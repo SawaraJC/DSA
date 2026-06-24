87343b0e0e80a7084d3453a14253abb5f8ed455a
SawaraJC <112013319+SawaraJC@users.noreply.github.com>
2026-06-24 23:00:46 +0530

    Time: 6 ms (69.77%), Space: 46.4 MB (47.29%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,4 +1,30 @@
-import java.util.HashSet;
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if (s.length() == 0) {
+//             return 0;
+//         }
+
+//         int max = 0; 
+//         int longest = 0;
+
+//         HashSet<Character> set = new HashSet<>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             if (set.add(s.charAt(i))) {
+//                 max++;
+//                 if (max > longest) {
+//                     longest = max;
+//                 }
+//             } else {
+//                 set.clear();
+//                 i = i - max; 
+//                 max = 0; 
+//             }
+//         }
+
+//         return longest;
+//     }
+// }
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
@@ -6,22 +32,18 @@ class Solution {
             return 0;
         }
 
-        int max = 0; 
-        int longest = 0;
-
         HashSet<Character> set = new HashSet<>();
+        int longest = 0;
+        int left = 0; 
 
-        for (int i = 0; i < s.length(); i++) {
-            if (set.add(s.charAt(i))) {
-                max++;
-                if (max > longest) {
-                    longest = max;
-                }
-            } else {
-                set.clear();
-                i = i - max; 
-                max = 0; 
+        for (int right = 0; right < s.length(); right++) {
+
+            while (!set.add(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
             }
+
+            longest = Math.max(longest, right - left + 1);
         }
 
         return longest;
